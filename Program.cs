f0d9cc1367515c83cba77e0177743c962f9d4925
Nanohz <38676579+Nanohz@users.noreply.github.com>
2018-08-12 16:41:39 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace VsApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems!");
+            Console.ReadLine();
+            Console.WriteLine("Remember, we are 'Accounting' on you!");
+            Console.ReadLine();
+            Console.WriteLine("Please hit enter to exit screen");
+            Console.ReadLine();
+        
+
+        }
+    }
+}
