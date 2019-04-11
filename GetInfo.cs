06b28d65b447504b7e6bf534ab44793a1bd62f6e
Nanohz <38676579+Nanohz@users.noreply.github.com>
2019-04-11 16:23:35 -0700

    Add files via upload

---
+++ GetInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projectGetUser.Models
+{
+    public class GetInfo
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+    }
+}
